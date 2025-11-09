d308adb5714e5ddfbc5c6a04c8c3a18c1fe2a04b
Sushant Shirsekar <63793660+sushantshirsekar@users.noreply.github.com>
2025-11-09 15:51:42 +0530

    Delete day_1_goal.py

--- day_1_goal.py
+++
@@ -1,9 +0,0 @@
-# ==================================================
-# Script Purpose : Combine your city and pet name
-#                  to generate band name
-# ==================================================
-
-print('Welcome to the Band Name Generator.')
-city_name = input('In which city you grew up in?\n')
-pet_name = input('What is your pet name?\n')
-print('Your band name could be ' + city_name + ' ' + pet_name)
