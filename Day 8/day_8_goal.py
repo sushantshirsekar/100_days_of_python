7dd486749f7c9b6dc699f49252b2062225553902
Sushant Shirsekar <63793660+sushantshirsekar@users.noreply.github.com>
2025-11-13 16:24:07 +0530

    Update day_8_goal.py

--- Day 8/day_8_goal.py
+++ Day 8/day_8_goal.py
@@ -10,19 +10,14 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 
 def caesar(original_text, shift_amount, encode_or_decode):
     output_text = ""
-
+    if encode_or_decode == "decode":
+        shift_amount *= -1
     for letter in original_text:
         if letter not in alphabet:
             output_text += letter
         else:
-            if encode_or_decode == "decode":
-                shifted_position = alphabet.index(letter) - shift_amount
-                if shifted_position < 0:
-                    shifted_position *= -1
-                    shifted_position = 26 - shifted_position
-            else:
-                shifted_position = alphabet.index(letter) + shift_amount
-                shifted_position %= len(alphabet)
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position = shifted_position % len(alphabet)
             output_text += alphabet[shifted_position]
     print(f"Here is the {encode_or_decode}d result: {output_text}")
 
@@ -33,14 +28,11 @@ while restart:
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
+    
     caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
-    restart_inp = input('''Type 'yes' if you want to go again. Otherwise, type 'no'. ''').lower()
-    if restart_inp == 'yes':
-        restart = True
-    else:
+    
+    restart_inp = input('''Type 'yes' if you want to go again. Otherwise, type 'no'. \n''').lower()
+    
+    if restart_inp == 'no':
         restart = False
-
-
-
-
-
+        print("Good Bye!")
