16c3b137737336d0f8e02d1ed8d707cb3cf4b4ca
Sushant Shirsekar <63793660+sushantshirsekar@users.noreply.github.com>
2025-11-14 16:38:50 +0530

    Restructuring the code

--- Day 9/day_9_goal.py
+++ Day 9/day_9_goal.py
@@ -1,36 +1,29 @@
 # TODO-1: Ask the user for input
+# TODO-2: Save data into dictionary {name: price}
+# TODO-3: Whether if new bids need to be added
+# TODO-4: Compare bids in dictionary
 import art
 
-show_art = True
+print(art.logo)
 
-if show_art:
-    print(art.logo)
+def find_highest_bidder(bidding_dictionary):
+    winner = max(bidding_dictionary, key=bidding_dictionary.get)
+    print(f"The winner is {winner} with bid {bidding_dictionary[winner]}")
 
-my_dict = {}
+bids = {}
 
-is_repeat = True
+should_continue = True
 
-while is_repeat:
+while should_continue:
     name = input("What's your name?: ")
     price = int(input("What is your bid: $"))
 
-    # TODO-2: Save data into dictionary {name: price}
-    my_dict[name] = price
+    bids[name] = price
 
-    # TODO-3: Whether if new bids need to be added
-    restart = input("Are there any other bidders? Type 'yes' or 'no': ").lower()
+    restart_input = input("Are there any other bidders? Type 'yes' or 'no': ").lower()
 
-    if restart == 'no':
-        is_repeat = False
-    else :
+    if restart_input == 'yes':
         print("\n" * 50)
-
-# TODO-4: Compare bids in dictionary
-max_bid = 0
-max_bid_name = ""
-for key in my_dict:
-    if max_bid < my_dict[key]:
-        max_bid = my_dict[key]
-        max_bid_name = key
-
-print(f"The winner is {max_bid_name} with bid {max_bid}")
+    else :
+        should_continue = False
+        find_highest_bidder(bids)
