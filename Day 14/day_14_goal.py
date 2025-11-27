701d02e6de3e5fcbaa4620b911ee6dfe43bf19d9
Sushant Shirsekar <63793660+sushantshirsekar@users.noreply.github.com>
2025-11-27 12:30:23 +0530

    Update day_14_goal.py

--- Day 14/day_14_goal.py
+++ Day 14/day_14_goal.py
@@ -1,50 +1,59 @@
-import art
-import game_data
+from art import logo, vs
+from game_data import data
 import random
 
-def print_comparison(first_data, second_data):
-    print(
-        f"Compare A: {first_data["name"]}, a {first_data["description"]}, from {first_data["country"]} with {first_data["follower_count"]} followers")
-    print(art.vs)
-    print(
-        f"Compare B: {second_data["name"]}, a {second_data["description"]}, from {second_data["country"]} with {second_data["follower_count"]} followers")
-
-def get_random_data():
-    return random.choice(game_data.data)
-
-# 1. Print project logo
-print(art.logo)
-
-# 3. Choose random data for A and B comparison
-a_data = get_random_data()
-b_data = get_random_data()
+def print_profile(dummy_data):
+    return (
+        f"{dummy_data["name"]}, a {dummy_data["description"]}, from {dummy_data["country"]} with {dummy_data["follower_count"]} followers")
 
 
+def check_user_guess(user_guess, a_followers, b_followers):
+    if a_followers > b_followers:
+        return user_guess == "a"
+    else:
+        return user_guess == "b"
 
-# 8. While loop to run until user gives correct answers
+
+# 3. Choose random data for B.
+b_data = random.choice(data)
+
+
+# 8. While loop to run until user gives correct answers.
 is_game_over = False
 score = 0
-while not is_game_over:
-    # 4. Print Compare A and B statements and also print 'vs' logo in between
-    print_comparison(a_data, b_data)
+while True:
+    # 1. Print project logo
+    print(logo)
 
-    # 5. Get User Input
+    # 7. Convert B to A and get new B.
+    a_data = b_data
+    if a_data == b_data:
+        b_data = random.choice(data)
+
+    # 9. Show user his/her score if he/she is right.
+    if score > 0:
+        print(f"You're right! Current Score : {score}")
+
+    # 4. Print game comparison statements.
+    print(f"Compare A : {print_profile(a_data)}")
+    print(vs)
+    print(f"Against B : {print_profile(b_data)}")
+
+    # 5. Get User Guess.
     user_input = input("Who has more followers? Type 'A' or 'B':  ").lower()
 
-    # 6. Decide score for user according to answer
-    if user_input == 'a' and a_data["follower_count"] > b_data["follower_count"] or user_input == 'b' and a_data["follower_count"] < b_data["follower_count"]:
+    # 6. Decide score for user according to answer.
+    is_user_correct = check_user_guess(user_input,a_data['follower_count'], b_data['follower_count'])
+    if is_user_correct:
         score += 1
     else:
         print("\n" * 20)
-        print(art.logo)
+        print(logo)
         print(f"Sorry that's wrong. Final Score: {score}")
         is_game_over = True
         break
-    # 7. Convert B to A and get new B
-    a_data = b_data
-    b_data = get_random_data()
-    if a_data == b_data:
-        b_data = get_random_data()
+
+
 
 
 
